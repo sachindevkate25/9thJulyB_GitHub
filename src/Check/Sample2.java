e421a3f5aa4392ab3e29862e93a632b203b11455
SACHIN <SACHIN@DESKTOP-GICICBH>
2022-12-13 11:11:34 +0530

    [SACHIN]:Sample2

---
+++ src/Check/Sample2.java
@@ -0,0 +1,23 @@
+package Check;
+
+public class Sample2 
+{
+	public static void main(String[]args)
+	{
+		String org="ABCD";
+		
+		String rev="";
+		
+		
+		for(int i=org.length()-1; i>=0; i--)
+		{
+			rev=rev+org.charAt(i);
+		}
+		
+		System.out.println(rev);
+		
+	}
+	
+	
+
+}
